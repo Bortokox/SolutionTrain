caa55974eeec6fbd14320dcd00943116323c3b12
Bortokox <bortokox@gmail.com>
2020-08-05 08:08:14 +0200

    First task

---
+++ Calculator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 15;
+            int b = 20;
+            int sum = a + b;
+
+            Console.WriteLine("Suma a i b wynosi {0}", sum);
+
+           
+        }
+    }
+}
---
+++ SolutionTraining/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SolutionTraining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your name");
+            string string1 = Console.ReadLine();
+            Console.Write("What is your surname");
+            string string2 = Console.ReadLine();
+
+            Console.WriteLine("Hello world nazywam się {0} {1} i uczę się programować w C#. ", string1, string2);
+
+
+        }
+    }
+}
